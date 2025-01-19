19ac1f9ca8b57786d6e9f3ec164ac96a94d2ef68
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2025-01-18 21:22:09 -0500

    Added password hashing with bcrypt

--- backend/src/main/java/com/gamethreads/gamethreads/service/UserService.java
+++ backend/src/main/java/com/gamethreads/gamethreads/service/UserService.java
@@ -2,6 +2,7 @@ package com.gamethreads.gamethreads.service;
 
 import java.util.List;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.gamethreads.gamethreads.dto.user.NewUserDTO;
@@ -11,6 +12,7 @@ import com.gamethreads.gamethreads.repository.UserRepository;
 @Service
 public class UserService {
   private UserRepository userRepository;
+  private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
 
   public UserService(UserRepository userRepository) {
     this.userRepository = userRepository;
@@ -21,8 +23,10 @@ public class UserService {
   }
 
   public User createUser(NewUserDTO newUserDTO) {
-    User newUser = new User(newUserDTO);
+    String hashedPassword = encoder.encode(newUserDTO.getPassword());
+    newUserDTO.setPassword(hashedPassword);
 
+    User newUser = new User(newUserDTO);
     return this.userRepository.save(newUser);
   }
 }
