c002e2251930dddc6168e985ae96824c8105a31c
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2025-01-18 22:14:37 -0500

    Created basic status 400 exception

---
+++ backend/src/main/java/com/gamethreads/gamethreads/exception/status/Status400Exception.java
@@ -0,0 +1,7 @@
+package com.gamethreads.gamethreads.exception.status;
+
+public class Status400Exception extends RuntimeException {
+  public Status400Exception(String msg) {
+    super(msg);
+  }
+}
