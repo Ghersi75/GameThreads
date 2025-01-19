2737b1dd19a17a9005c4b2aee04b51dacd8c3150
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2025-01-18 20:13:25 -0500

    Added spring boot initial project files with /test/ endpoint. Commented out spring security dependencies for testing and added h2 for testing as well.

---
+++ backend/src/main/java/com/gamethreads/gamethreads/GameThreadsApplication.java
@@ -0,0 +1,11 @@
+package com.gamethreads.gamethreads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameThreadsApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(GameThreadsApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/com/gamethreads/gamethreads/GameThreadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamethreads.gamethreads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameThreadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
