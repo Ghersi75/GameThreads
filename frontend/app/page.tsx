73638ab01330749931cda3e869eeee8c436e9c18
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2025-01-29 13:33:06 -0500

    Created basic landing page with columns and picsum images

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,39 @@
-import Image from "next/image";
+import axios from "axios";
+import Image from "@/components/Home/Image"
 
-export default function Home() {
+export interface APIResponse {
+  id: number,
+  width: number,
+  height: number,
+  url: string,
+  download_url: string
+}
+// "id": "102",
+// "author": "Ben Moore",
+// "width": 4320,
+// "height": 3240,
+// "url": "https://unsplash.com/photos/pJILiyPdrXI",
+// "download_url": "https://picsum.photos/id/102/4320/3240"
+// },
+
+export default async function Home() {
+  let pics: APIResponse[] = [];
+  try {
+    const response = await axios.get("https://picsum.photos/v2/list?page=2&limit=200");
+    pics = response.data; // Access the data property where the array is located
+  } catch (err) {
+    console.error(err);
+  }
+
+  console.log(pics)
+
   return (
-    <div className="h-svh w-svw flex justify-center items-center">
-      Nice
+    <div className="min-h-svh w-svw gap-8 columns-6 bg-black text-white p-8">
+      {pics.map((pic) => {
+        return (
+          <Image image={pic} key={pic.id} />
+        )
+      })}
     </div>
   );
 }
